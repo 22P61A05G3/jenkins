980b7e56746cd01f8e2738da93f46263f9f1e949
drsaab1313 <arjun.cse1313@gmail.com>
2025-03-02 15:11:36 +0530

    first

---
+++ reg.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("form.html")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    age = request.form.get("age")
+    return render_template("result.html", name=name, email=email, age=age)
+
+if __name__ == '__main__':
+    app.run(debug=True)
